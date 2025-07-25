4311ecd7fabf9727bbe32f3739f7e7734d699201
Ashfa Nisthar <120776447+AshfaNisthar@users.noreply.github.com>
2025-07-25 23:19:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+import cors from 'cors';
+
+
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Database connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1); // Exit the process with failure
+  });
+
+// Middleware to parse JSON requests
+app.use(express.json());
+app.use(cors());
+
+// Basic route for testing
+app.get("/", (req, res) => {
+  res.send("Welcome to the Note App API");
+});
+
+// Import and use the note routes
+import noteRouter from "./Routes/noteRoutes.js";
+app.use("/api/notes", noteRouter);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
